d0ac5f8271c04f5b86a34c8e054c3d5fa139ee3f
mikaelangelo12 <mikaelangelo12@gmail.com>
2021-09-13 00:26:09 -0300

    Iniciando o Front

--- medical/src/app/app.module.ts
+++ medical/src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeModule } from './home/home.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+
+
   ],
   imports: [
-
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
-  bootstrap: [AppComponent, HomeComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
